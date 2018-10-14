1a17e97fe3b8bda0cbe521502572d3661eb6e001
paulb <paulb@DESKTOP-0QKMO3J>
2018-10-13 22:08:38 -0500

    Funciona revertir cambios

--- Deber/src/deber/Fabiana.java
+++ Deber/src/deber/Fabiana.java
@@ -13,6 +13,5 @@ public class Fabiana {
     public static void main(String[] args) {
         System.out.println("Hola Compañeros");
         System.out.println("Hola. Att: Jessica");
-        System.out.println("Hola Fabu...");
     }
 }
