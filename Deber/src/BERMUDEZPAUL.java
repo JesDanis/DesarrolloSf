f943430592e911e6207b733133282ff7c4d56f69
Estudiante <Estudiante@LAB06_PC06>
2018-10-29 11:30:20 -0500

    Modificado por Lg

--- Deber/src/BERMUDEZPAUL.java
+++ Deber/src/BERMUDEZPAUL.java
@@ -14,7 +14,7 @@ public class BERMUDEZPAUL {
     
     
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Gio");
         System.out.println("hola paul funciona");
     }
 }
