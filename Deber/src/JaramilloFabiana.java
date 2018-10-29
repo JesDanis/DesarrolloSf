8750a1630382e25ceac5988dcb02423e28ed0d7d
Estudiante <Estudiante@LAB06_PC06>
2018-10-29 11:30:57 -0500

    Modificado por Lg

--- Deber/src/JaramilloFabiana.java
+++ Deber/src/JaramilloFabiana.java
@@ -11,6 +11,7 @@
  */
 public class JaramilloFabiana {
     public static void main(String[] args) {
+        System.out.println("Gio");
         System.out.println("FABIANA FUNCIONO _???");
     }
 }
